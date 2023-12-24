b31d7965ab306e8dbf569fd2535e8d13c4b9e0d0
Mohit Bhandare <mohitbhandare2001@gmail.com>
2023-12-24 09:28:24 +0530

    new project setup

---
+++ lpt-liftPleaseTaxi/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-white bg-slate-600 text-center text-3xl ">
+        This is LiftPlease Taxi app.
+      </h1>
+    </>
+  );
+}
+
+export default App;
